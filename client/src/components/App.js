0133e8b53f859b10a20a54e5770fa48ef22a7b95
vincentbaylon <vincentbaylon@gmail.com>
2021-09-11 13:52:25 -0700

    Added image and form inputs

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,16 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+
+import Login from './Login'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Guardians: baNaNa</h1>
+    <div>
+      <CssBaseline />
+      <Container fixed>
+        <Login />
+      </Container>
     </div>
   );
 }
---
+++ client/src/components/Login.js
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import { Grid } from '@material-ui/core'
+import { TextField } from '@material-ui/core'
+import { Typography } from '@material-ui/core'
+import { Button } from '@material-ui/core'
+import Logo from './assets/guardians-logo.png'
+
+const imgStyle = {
+    width: '100%',
+    maxWidth: '750px'
+}
+
+function Login() {
+    const [login, setLogin] = useState(false)
+
+    const handleClick = () => {
+        setLogin(login => !login)
+    }
+
+    return (
+        <>
+            <form>
+                <Grid container spacing={4} justifyContent="center" alignItems="center" direction="column" style={{ minHeight: '100vh' }}>
+                    <Grid item>
+                        <img src={Logo} style={imgStyle} />
+                    </Grid>
+
+                    <Grid item>
+                        <Typography variant="h2" color="textPrimary">
+                            {login ? "Log In" : "Sign Up"}
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <TextField id="username" label="Username" defaultValue="" />
+                    </Grid>
+                    <Grid item>
+                        <TextField id="password" label="Password" defaultValue="" />
+                    </Grid>
+                    <Grid item>
+                        <Button variant="contained" color="secondary">
+                            {login ? "Log In" : "Sign Up"}
+                        </Button>
+                    </Grid>
+                    <Grid item direction="row">
+                        {login ? "No account?" : "Already have an account?"}
+                        <Button variant="text" color="primary" size="small" onClick={handleClick}>
+                            {login ? "Sign Up" : "Log In"}
+                        </Button>
+                    </Grid>
+                </Grid>
+            </form>
+        </>
+    )
+}
+
+export default Login
