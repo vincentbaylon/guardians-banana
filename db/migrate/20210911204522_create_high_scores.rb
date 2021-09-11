00685adfa58c8e48cf0e69f3f3971ce68ae4c6f3
tjray-dev <64376864+tjray-dev@users.noreply.github.com>
2021-09-11 15:32:30 -0600

    Klass model built

--- db/migrate/20210911204522_create_high_scores.rb
+++ db/migrate/20210911204522_create_high_scores.rb
@@ -1,7 +1,7 @@
 class CreateHighScores < ActiveRecord::Migration[6.1]
   def change
     create_table :high_scores do |t|
-      t.belongs_to :user_id, null: false, foreign_key: true
+      t.belongs_to :user, null: false, foreign_key: true
       t.integer :score
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_11_204522) do
+ActiveRecord::Schema.define(version: 2021_09_11_212443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2021_09_11_204522) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_high_scores_on_user_id"
+  end
+
+  create_table "klasses", force: :cascade do |t|
+    t.string "klass_name"
+    t.integer "attack_bonus"
+    t.integer "defense_bonus"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
