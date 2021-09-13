350cf7c4795029cead74b6ab65b77c90c72b1f1e
tjray-dev <64376864+tjray-dev@users.noreply.github.com>
2021-09-12 21:00:48 -0600

    character controller actions

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -3,18 +3,25 @@ class CharactersController < ApplicationController
   before_action :find_character, except: :index
   
   def index
+    render json: Character.all, status: :ok
   end
 
   def show
+    render json: @character, status: :ok
   end
 
   def create
+    render json: Characte.create!(character_params), status: :created 
   end
 
   def update
+    @character.update(character_params)
+    render json: @character, status: :accepted
   end
 
   def destroy
+    @character.destroy
+    head :no_content
   end
 
   private
